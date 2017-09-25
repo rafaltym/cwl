dc2610baf3cd73fc84c18c4d9b9adf1981d9b846
Rafał <rafaltym@wp.pl>
2017-09-25 16:58:13 +0200

    rozszerzenie programu

--- projekt2/main.cpp
+++ projekt2/main.cpp
@@ -5,10 +5,17 @@ using namespace std;
 
 int main()
 {
-    string imie ="Jan" ;
-    string nazwisko = "Kowalski";
-    int rok_urodzenia = 2000;
-    int aktualny_rok = 2017;
+    string imie ;
+    string nazwisko ;
+    int rok_urodzenia;
+    int aktualny_rok=2017;
+    cout<< "podaj swoje imie"<<endl;
+    cin>>imie;
+    cout<< "podaj swoje nazwisko"<<endl;
+    cin>>nazwisko;
+    cout<< "podaj swoj rok urodzenia"<<endl;
+    cin>>rok_urodzenia;
+
 
     cout<<"Witam, nazywam sie "<< imie <<" "<< nazwisko<<" i mam "<<aktualny_rok - rok_urodzenia<<" lat";
     return 0;
